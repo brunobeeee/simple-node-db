c8d8c5929ec39cb4bab21182cc71ac8e7c9a94c1
brunobeeee <github@bbergmann.de>
2023-07-17 19:04:47 +0000

    Initial commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+const mysql = require('mysql.js');
+
+dotenv.config({ path: '../../.env'})
+
+// Connect to database
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+
+// Connect
+db.connect(function(err) {
+  if (err) {
+    console.error('Error whilst connecting to database', err);
+    return;
+  }
+  console.log('Connected with MySQL database');
+});
